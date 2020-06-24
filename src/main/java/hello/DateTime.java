df46170bcfce8a4492db96fd24bc2701be6ebe9f
anand <anand123hai@gmail.com>
2020-06-24 22:25:24 +0530

    commented DateTime.java min,day,month,year

--- src/main/java/hello/DateTime.java
+++ src/main/java/hello/DateTime.java
@@ -8,10 +8,10 @@ public class DateTime {
   public int dateTime(){
   GregorianCalendar time = new GregorianCalendar();
   int hour = time.get(Calendar.HOUR_OF_DAY);
-  int min = time.get(Calendar.MINUTE);
-  int day = time.get(Calendar.DAY_OF_MONTH);
-  int month = time.get(Calendar.MONTH) + 1;
-  int year = time.get(Calendar.YEAR);
+  //int min = time.get(Calendar.MINUTE);
+  //int day = time.get(Calendar.DAY_OF_MONTH);
+  //int month = time.get(Calendar.MONTH) + 1;
+  //int year = time.get(Calendar.YEAR);
   
   System.out.println(hour);
   return hour;
